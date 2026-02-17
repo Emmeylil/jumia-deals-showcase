521f4add9a39037977d9fc1c79e8b8c5b99c1ff4
emmeylil <emmanuel.salako@jumia.com>
2026-02-17 11:37:07 +0100

    update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -73,11 +73,11 @@ const Index = () => {
               <div className="absolute bottom-[-50px] left-[-50px] w-40 h-40 bg-[#009FE3]/10 rounded-full blur-3xl pointer-events-none" />
 
               <div className="mb-8 z-10">
-                <svg viewBox="0 0 160 40" className="h-16 w-auto" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M18.8 30.6c-4.6 0-7.8-1.5-9.6-4.5V11.2H0v15.2c0 4.1 1.2 7.4 3.7 9.8 2.5 2.5 6 3.7 10.4 3.7 4.5 0 8.3-1.4 11.2-4.1l-6.5-5.2zM48 11.2h-9.2v17.6c0 4.3-2.1 6.5-6.3 6.5-4 0-6-2.2-6-6.5V11.2h-9.2v18.4c0 4.1 1.1 7.2 3.2 9.2 2.2 2.1 5.2 3.1 9.2 3.1 3.5 0 6.4-1 8.6-3 1.1-.9 1.9-2 2.6-3.3v5.5H48V11.2zM75.2 11.2h-8.8l-5.6 19.5-5.5-19.5h-8.9v29.7h9.2V22l-5.6-10.8h.4l5.3 18.2 5.5-18.2h.4L66 22v18.9h9.2V11.2zM80.2 11.2h9.2v29.7h-9.2zM118.8 30.6l-6.5-5.1c-2.4 2.1-4.7 3.2-6.9 3.2-4 0-6-2.2-6-6.5V11.2H90.2v18.4c0 4.1 1.1 7.2 3.2 9.2 2.2 2.1 5.2 3.1 9.2 3.1 4.4 0 7.9-1.2 10.7-3.7 1.8-1.7 3.7-4.4 5.5-7.6z" fill="#282828" />
-                  <circle cx="140" cy="20" r="18" className="fill-[#FF9900]" />
-                  <path d="M140 10l2.5 7.5h8l-6.5 4.5 2.5 7.5-6.5-4.5-6.5 4.5 2.5-7.5-6.5-4.5h8z" className="fill-white" />
-                </svg>
+                <img
+                  src="https://ng.jumia.is/cms/jumia_logo_small.png"
+                  alt="Jumia Logo"
+                  className="h-16 w-auto object-contain"
+                />
               </div>
 
               <div className="relative z-10">
