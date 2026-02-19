16e728ad956341e0210b088585e8b15641420127
emmeylil <emmanuel.salako@jumia.com>
2026-02-19 13:08:15 +0100

    update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,19 @@
-import { initializeApp } from "@firebase/app";
+import { initializeApp } from "firebase/app";
 import {
   initializeFirestore,
   persistentLocalCache,
   persistentMultipleTabManager
-} from "@firebase/firestore";
-import { getAuth } from "@firebase/auth";
-import { getStorage } from "@firebase/storage";
+} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAK57O6YKG17sXSw0GovdLFN-B_FLrn19M",
-  authDomain: "jumia-e-catalog.firebaseapp.com",
-  projectId: "jumia-e-catalog",
-  storageBucket: "jumia-e-catalog.firebasestorage.app",
-  messagingSenderId: "776751698383",
-  appId: "1:776751698383:web:e18138daae9c4564a402ba"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
